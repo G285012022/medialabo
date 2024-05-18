236a290bd94316f70b6fc7b159baffa8e7fe8f4b
G285012022 <228501@st.takaushoku-u.ac.jp>
2024-05-18 13:55:46 +0900

    変更

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,17 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);//「八王子市館町」を出力
+console.log(campus.buildingD[0]);//「D101」を出力
+console.log(campus.buildingD[1]);//「D102」を出力
+console.log(campus.buildingD[1]);//「D103」を出力
+console.log(campus.buildingD[2]);//「D201」を出力
+console.log(campus.buildingD[3]);//「D202」を出力
+console.log(campus.buildingD[4]);//「D203」を出力
+console.log(campus.buildingD[5]);//「D204」を出力
+console.log(campus.buildingD[6]);//「D205」を出力
+
+console.log(gakka[0].name);//「機械システム工学科」を出力
+console.log(gakka[1].name);//「電子システム工学科」を出力
+console.log(gakka[2].name);//「情報工学科」を出力
+console.log(gakka[3].name);//「デザイン学科」を出力
