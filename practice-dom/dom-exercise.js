bb43a5040b559abdb9882dea123f945167377fb6
G285012022 <228501@st.takaushoku-u.ac.jp>
2024-05-25 18:46:46 +0900

    変更

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -12,17 +12,68 @@ let data = [
 	{name:'沖縄', lat:26.2125, lng:127.68111}
 ];
 //// 注意: 以上は編集しないこと!
+
+
+
 // 練習4-2 メッセージ追加プログラム
 let h2 = document.querySelector('h2#ex42');
 let p = document.createElement('p');
-p.textContent ='style = "text-emphasis: sesame green;">写真表と都市の緯度軽度のページです';
+p.textContent ='写真表と都市の緯度軽度のページです';
 h2.insertAdjacentElement('afterend', p);
+p.style.textEmphasis = 'sesame green';
+
+
+
+
 
 // 練習4-3 写真表作成プログラム
+let phototable = document.querySelector('div#phototable');
+
+let imgtaro = document.createElement('img');        //img要素を作成
+imgtaro.setAttribute('src', 'taro.png');            //img要素のsrc属性を設定
+let p1 = document.createElement('p');               //p要素を作成                               
+p1.insertAdjacentElement('beforeend', imgtaro);     //p要素の中の最後に img 要素を追加
+phototable.insertAdjacentElement('beforeend', p1);  //div#phototable の中の最後に p 要素を追加
+
+let imgjiro = document.createElement('img');        //img要素を作成
+imgjiro.setAttribute('src', 'jiro.png');            //img要素のsrc属性を設定
+let p2 = document.createElement('p');               //p要素を作成                               
+p2.insertAdjacentElement('beforeend', imggiro);     //p要素の中の最後に img 要素を追加
+phototable.insertAdjacentElement('beforeend', p2);  //div#phototable の中の最後に p 要素を追加
+
+let imghanako = document.createElement('img');      //img要素を作成
+imghanako.setAttribute('src', 'hanako.png');        //img要素のsrc属性を設定
+let p3 = document.createElement('p');               //p要素を作成                               
+p3.insertAdjacentElement('beforeend', imghanako);   //p要素の中の最後に img 要素を追加
+phototable.insertAdjacentElement('beforeend', p3);  //div#phototable の中の最後に p 要素を追加
 
 
 // 練習4-4 箇条書き削除プログラム
 
 
+
+let loc = document.querySelectorAll('ul#location > li');
+for (let liElement of loc) {
+    liElement.remove();
+}
+
+
+
 // 練習4-5 箇条書き追加プログラム
+
+
+
+
+let kei = document.querySelector('ul#location');
+for (let i = 0; i < data.length; i++) {
+    let city = data[i];
+    let listItem = document.createElement('li');
+    listItem.textContent = city.name + ' ... 緯度: ' + city.lat + ', 経度: ' + city.lng;
+    kei.appendChild(listItem);
+}
+
+
+
+
+
 
